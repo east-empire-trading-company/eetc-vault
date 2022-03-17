2ce2f7b48dd7eece89645d09273da5c211af0a19
dovedan <dovedanmilosh@gmail.com>
2022-03-18 00:40:33 +0100

    Add Feature flags tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,3 +22,38 @@ def test_get_request_method_not_allowed(client):
 
     # then
     assert rv.status_code == 405
+
+
+def test_valid_post_request_feature_flags(client):
+    # given
+    post_data = {"feature": "test_feature", "active": "true"}
+    api_endpoint = "/api/config/feature_flags"
+
+    # when
+    rv = client.post(api_endpoint, json=post_data)
+
+    # then
+    assert rv.status_code == 200
+
+
+def test_bad_request_feature_flags(client):
+    # given
+    post_data = {"feature": "123", "active": "true"}
+    api_endpoint = "/api/config/feature_flags"
+
+    # when
+    rv = client.post(api_endpoint, json=post_data)
+
+    # then
+    assert rv.status_code == 400, {"ERROR": "123 does not exist!"}
+
+
+def test_get_request_method_not_allowed_feature_flags(client):
+    # given
+    api_endpoint = "/api/config/feature_flags"
+
+    # when
+    rv = client.get(api_endpoint)
+
+    # then
+    assert rv.status_code == 405
